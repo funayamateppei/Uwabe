d53ffd72dd3745b4ea8df4ce707b33de1de0c210
funayamateppei <t.funayama0217@gmail.com>
2022-11-17 23:15:08 +0900

    add

---
+++ android/app/src/main/kotlin/com/example/js_champion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.js_champion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: '',
+      home: LoginPage(),
+    );
+  }
+}
